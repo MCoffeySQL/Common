56715bd1dd1606bd89eca932f9e41c7053b2e655
Michael <mcoffeysql@gmx.com>
2022-06-20 13:12:04 -0400

    Create dbo.symbols-create.sql

---
+++ Tables/dbo.symbols-create.sql
@@ -0,0 +1,15 @@
+USE common;
+GO
+
+DROP TABLE IF EXISTS dbo.symbols;
+GO
+
+CREATE TABLE dbo.symbols(
+  id SMALLINT NOT NULL,
+  symbol VARCHAR(24) NULL,
+  hex VARCHAR(4) NULL,
+  html VARCHAR(8) NULL,
+  morse VARCHAR(24) NULL,
+  [description] VARCHAR(64)
+  );
+GO
