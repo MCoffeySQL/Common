19cda54306fea5bf296395241f1ed6c29459d702
Michael <mcoffeysql@gmx.com>
2022-12-10 12:10:46 -0500

    Create dbo.backup_medical.sql

---
+++ Procedures/dbo.backup_medical.sql
@@ -0,0 +1,55 @@
+USE Common;
+GO
+
+DROP PROCEDURE IF EXISTS dbo.backup_medical;
+GO
+
+CREATE OR ALTER PROCEDURE dbo.backup_medical
+  @backupType NVARCHAR(24)
+AS
+BEGIN
+  --Usage Example:
+  --  EXEC dbo.backup_medical 'Full'
+  --  EXEC dbo.backup_medical 'Differential'
+
+  DECLARE 
+    @databaseName NVARCHAR(128) = 'medical',
+    @destinationPath NVARCHAR(128) = 'D:\SQL Server\Backup\',
+    @destinationFileName NVARCHAR(128),
+    @destinationPathFileName NVARCHAR(255),
+    @backupName NVARCHAR(128)
+
+  SET @destinationFileName = @databaseName + '_' + @backupType + '_' + CAST(YEAR(GETDATE()) AS NVARCHAR(4)) + RIGHT('0' + CAST(MONTH(GETDATE()) AS NVARCHAR(2)),2) + RIGHT('0' + CAST(DAY(GETDATE()) AS NVARCHAR(2)),2) + '.bak'
+  SET @destinationPathFileName = @destinationPath + @destinationFileName
+  SET @backupName = @databaseName + '-' + @backupType + ' Database Backup'
+
+  IF @backupType = 'Full'
+  BEGIN
+    BACKUP DATABASE @databaseName
+     TO  DISK = @destinationPathFileName
+    WITH 
+      NOFORMAT, 
+      NOINIT,  
+      NAME = @backupName, 
+      SKIP, 
+      NOREWIND,
+      NOUNLOAD,  
+      STATS = 10
+  END 
+
+  IF @backupType = 'Differential'
+  BEGIN
+    BACKUP DATABASE @databaseName
+    TO DISK = @destinationPathFileName
+    WITH DIFFERENTIAL,
+      NOFORMAT, 
+      NOINIT,
+      NAME = @backupName,
+      SKIP,
+      NOREWIND,
+      NOUNLOAD,
+      STATS = 10
+
+  END
+END
+GO
