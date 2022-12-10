347d22adaaa97c575a10889276735e76e2928140
Michael <mcoffeysql@gmx.com>
2022-12-10 10:56:32 -0500

    Update dbo.symbols-alter.sql

--- Tables/dbo.symbols-alter.sql
+++ Tables/dbo.symbols-alter.sql
@@ -9,9 +9,3 @@ ALTER TABLE dbo.symbols
 ADD CONSTRAINT pk_symbols
 PRIMARY KEY CLUSTERED (id);
 
---Unique Constraint
-ALTER TABLE dbo.symbols
-DROP CONSTRAINT IF EXISTS unq_symbol;
-ALTER TABLE dbo.symbols
-ADD CONSTRAINT unq_symbol
-UNIQUE (symbol);
